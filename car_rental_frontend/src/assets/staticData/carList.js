d5f8b58e9f7d7244f50210e9436bfadba9a4c35d
jay parmar <jay.m.parmar2001@gmail.com>
2023-01-19 04:25:03 +0530

    final commit

--- car_rental_frontend/src/assets/staticData/carList.js
+++ car_rental_frontend/src/assets/staticData/carList.js
@@ -2,10 +2,32 @@ export const carListStatic = [
     {
         id:1,
         car_image:"carImg1",
-        car_name:"maruti x1",
+        car_name:"Hyundai GRAND i1",
         car_number:"Gj03 LL 8989",
         car_available_status:true,
         car_color:"white",
+        car_seat_capicity:4,
+        rent_per_day:350,
+        car_varient:"petrol",
+        car_engine_detail:"1.2 Kappa Dual VTVT BS6 Petrol Engine",
+        car_other_information:"Hyundai GRAND i10 NIOS comes with Wonder Warranty options of upto 5 years Std. Customer can choose any warranty option as per his driving requirement at the time of new vehicle delivery. From a strong body structure to Standard Dual airbags and ABS with EBD, maximum care has been taken to integrate a plethora of safety features",
+        car_issue_date:"",
+        car_return_date:"",
+        current_booking_detail:{
+          name:"",
+          phone_number:"",
+          car_issue_date:"",
+          car_return_date:""
+        }
+        
+    },
+    {
+        id:2,
+        car_image:"carImg2",
+        car_name:"Tata harrier",
+        car_number:"Gj03 LL 8989",
+        car_available_status:true,
+        car_color:"red",
         car_seat_capicity:4,
         rent_per_day:350,
         car_varient:"petrol",
@@ -22,12 +44,56 @@ export const carListStatic = [
         
     },
     {
-        id:2,
-        car_image:"carImg1",
-        car_name:"maruti x1",
+        id:3,
+        car_image:"carImg3",
+        car_name:"Hyundai Creta",
         car_number:"Gj03 LL 8989",
         car_available_status:true,
-        car_color:"white",
+        car_color:"black",
+        car_seat_capicity:4,
+        rent_per_day:350,
+        car_varient:"petrol",
+        car_engine_detail:"1.2 Kappa Dual VTVT BS6 Petrol Engine",
+        car_other_information:"Hyundai GRAND i10 NIOS comes with Wonder Warranty options of upto 5 years Std. Customer can choose any warranty option as per his driving requirement at the time of new vehicle delivery. From a strong body structure to Standard Dual airbags and ABS with EBD, maximum care has been taken to integrate a plethora of safety features",
+        car_issue_date:"25/01/2020",
+        car_return_date:"27/01/2020",
+        current_booking_detail:{
+          name:"Jon Doe",
+          phone_number:"+91 7905654483",
+          car_issue_date:"25/01/2020",
+          car_return_date:"27/01/2020"
+        }
+        
+    },
+    {
+        id:4,
+        car_image:"carImg4",
+        car_name:"Hyundai verna",
+        car_number:"Gj03 LL 8989",
+        car_available_status:true,
+        car_color:"black",
+        car_seat_capicity:4,
+        rent_per_day:350,
+        car_varient:"petrol",
+        car_engine_detail:"1.2 Kappa Dual VTVT BS6 Petrol Engine",
+        car_other_information:"Hyundai GRAND i10 NIOS comes with Wonder Warranty options of upto 5 years Std. Customer can choose any warranty option as per his driving requirement at the time of new vehicle delivery. From a strong body structure to Standard Dual airbags and ABS with EBD, maximum care has been taken to integrate a plethora of safety features",
+        car_issue_date:"25/01/2020",
+        car_return_date:"27/01/2020",
+        current_booking_detail:{
+          name:"Jon Doe",
+          phone_number:"+91 7905654483",
+          car_issue_date:"25/01/2020",
+          car_return_date:"27/01/2020"
+        }
+        
+    },,
+    {
+        id:5,
+        car_image:"carImg5",
+        car_name:"Hyundai i20",
+        car_number:"Gj03 LL 8989",
+        car_available_status:true,
+        car_color:"red",
         car_seat_capicity:4,
         rent_per_day:350,
         car_varient:"petrol",
